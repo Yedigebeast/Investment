6271b4a0a8ea7efed3d7b419294afd0df9f92f4e
Yedige Ashirbek <edige11020@gmail.com>
2023-02-13 14:43:16 +0600

    Initial Commit

---
+++ Investment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Investment
+//
+//  Created by Yedige Ashirbek on 13.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
